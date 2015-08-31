03fd69c754f914e88ec84455674524e665c8d0bb
Kim Christensen <kich@mvno.dk>
2015-08-31 14:42:01 +0200

    Handle optional version and fallback

--- src/Okanshi.Dashboard/GetMetrics.cs
+++ src/Okanshi.Dashboard/GetMetrics.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Okanshi.Dashboard.Models;
 
 namespace Okanshi.Dashboard
@@ -14,14 +16,28 @@ namespace Okanshi.Dashboard
 	{
 		public IEnumerable<Metric> Deserialize(string response)
 		{
-			var deserializeObject = JsonConvert.DeserializeObject<IDictionary<string, dynamic>>(response);
-			return deserializeObject
-				.Select(x => new Metric
-				{
-					Name = x.Key,
-					Measurements = x.Value.measurements.ToObject<IEnumerable<Measurement>>(),
-					WindowSize = x.Value.windowSize.ToObject<float>()
-				});
+			var jObject = JObject.Parse(response);
+			JToken versionToken;
+			jObject.TryGetValue("version", out versionToken);
+			var version = "0";
+			if (versionToken != null && versionToken.HasValues)
+			{
+				version = versionToken.Value<string>();
+			}
+
+			if (version.Equals("0", StringComparison.OrdinalIgnoreCase))
+			{
+				var deserializeObject = JsonConvert.DeserializeObject<IDictionary<string, dynamic>>(response);
+				return deserializeObject
+					.Select(x => new Metric
+					{
+						Name = x.Key,
+						Measurements = x.Value.measurements.ToObject<IEnumerable<Measurement>>(),
+						WindowSize = x.Value.windowSize.ToObject<float>()
+					});
+			}
+
+			return Enumerable.Empty<Metric>();
 		} 
 	}
 }
